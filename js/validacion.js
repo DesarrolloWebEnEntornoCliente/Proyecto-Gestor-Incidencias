5c25c9165ffb042656269ac5adb8e289878f721e
jjusdadoiturralde <55576431+jjusdadoiturralde@users.noreply.github.com>
2020-05-28 09:45:17 +0200

    Add files via upload

--- js/validacion.js
+++ js/validacion.js
@@ -4,6 +4,7 @@ var arrayIncidencias= [];
 
 function inicio() {
 
+   //Evento que afecta al boton añadir 
     document.getElementById('añadir').addEventListener('click', validacion);
 }
 
@@ -22,6 +23,7 @@ function validacion(evento) {
         console.log("Gravedad: " + gravedad);
         console.log("Responsable: " + responsable);
 
+        //Deja el label de error vacio tras la comprobación
         document.getElementById('errorIncidencia').innerHTML = "";
 
         //Información de los datos introducidos con un array a traves de la consola, usando objetos.
@@ -36,11 +38,74 @@ function validacion(evento) {
         //vacia los campos
         document.getElementById('incidencia').value = '';
         document.getElementById('responsable').value = '';
+        
+        listarDOM();
 
     } else {
+        
+        //Mensaje de error cuando el campo descripcion obligatorio esta vacio
 
         document.getElementById('errorIncidencia').innerHTML = "Rellene el campo descripcion";
         console.log("Rellene el campo descripción");
     }
 
 }
+
+function listarDOM(){
+    document.getElementById('mostrar').innerHTML = '';
+    
+    arrayIncidencias.forEach(elemento => {
+        document.getElementById('mostrar').appendChild(elemento.creacionDom());
+    });
+    
+}
+
+function borrarIncidencia(evento){
+    
+    console.log("borrarIncidencia(evento)");
+    
+    let boton = evento.target;
+    let idDiv = boton.parentElement.id;
+    var arrayID;
+
+    arrayIncidencias.forEach(elemento => {
+
+        if(idDiv == elemento.id) arrayID = elemento;
+
+    });
+    
+    
+    
+    
+    arrayIncidencias.splice(arrayIncidencias.indexOf(arrayID), 1);
+
+    boton.parentElement.remove();
+    
+}
+
+function cerrarIncidenciaDOM(evento){
+    
+    let boton = evento.target;
+    let idDiv = boton.parentElement.id;
+    var arrayID;
+    
+    arrayIncidencias.forEach(elemento => {
+
+        if(idDiv == elemento.id) arrayID = elemento;
+
+    });
+    
+    arrayID.abierta=false;
+    
+    
+    
+
+    boton.parentElement.remove(); 
+    
+}
+
+
+    
+    
+    
+    
