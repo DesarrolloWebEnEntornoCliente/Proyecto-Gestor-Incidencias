4285feb91f63579546a8ec9e8d28e49db86ead75
jjusdadoiturralde <55576431+jjusdadoiturralde@users.noreply.github.com>
2020-05-28 09:48:24 +0200

    Errores y ordenar

--- js/app.js
+++ js/app.js
@@ -10,36 +10,81 @@ function contadorIncidencias(empezar) {
     }
 }
 
-class Incidencia{
-    constructor(descripcion,gravedad,asignada,abierta) {
-        
-        this.id=nuevaIncidencia();
-        this.descripcion=descripcion;
-        this.gravedad=gravedad;
-        this.asignada= new Responsable(asignada,this.id,idResponsable);
-        this.abierta=false;
+class Incidencia {
+    constructor(descripcion, gravedad, asignada = null, abierta) {
+
+        this.id = nuevaIncidencia();
+        this.descripcion = descripcion;
+        this.gravedad = gravedad;
+        this.asignada = new Responsable(asignada, this.id, idResponsable);
+        this.abierta = false;
     }
-    
-    
-    
-    asignarResponsable(responsable){
-        
-        
-        this.asignada=responsable;
+
+
+
+    asignarResponsable(responsable) {
+
+
+        this.asignada = responsable;
     }
-    
-    quitarResponsable(){
-        this.asignada=null;
-        
+
+    quitarResponsable() {
+        this.asignada = null;
+
     }
-    
-    cerrarIncidencia(){
-        this.abierta=false;
-        
+
+    cerrarIncidencia() {
+        this.abierta = false;
+
     }
-    
-     abrirIncidencia(){
-         this.abierta=true; 
+
+    abrirIncidencia() {
+        this.abierta = true;
+    }
+
+    //
+    creacionDom() {
+        
+        
+        let div = document.createElement('div');
+
+        
+        let incidencia = document.createElement('h2');
+        incidencia.innerHTML ="Incidencia: "+this.descripcion;
+        div.appendChild(incidencia);
+
+        let gravedad = document.createElement('p');
+        gravedad.innerHTML = "Gravedad: "+this.gravedad;
+
+        div.appendChild(gravedad);
+
+        let responsable = document.createElement('p');
+        responsable.innerHTML = "Responsable: "+this.asignada.nombre;
+        div.appendChild(responsable);
+        
+        let botonCerrar = document.createElement('button');
+        botonCerrar.innerHTML = "Cerrar";
+        botonCerrar.setAttribute('class', 'cerrar')
+        botonCerrar.addEventListener('click', cerrarIncidenciaDOM);
+        
+        div.appendChild(botonCerrar);
+        
+        
+         let botonEliminar = document.createElement('button');
+        botonEliminar.innerHTML = "Eliminar";
+        botonEliminar.setAttribute('class', 'eliminar')
+        botonEliminar.addEventListener('click', borrarIncidencia);
+        
+        div.appendChild(botonEliminar);
+        
+        
+        div.setAttribute('id', this.id);
+
+
+        
+
+
+        return div;
     }
        
 }
